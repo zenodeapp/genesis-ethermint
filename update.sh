7e008d70331a30b06715f502e339b5e8aa964398
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-04-13 11:54:14 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,6 +13,17 @@ cat << "EOF"
 	 \______/  \_______/|__/  |__/ \_______/|_______/ |__/|_______/       |________/|______/
 	 
 	 
+	                               /$$             /$$             /$$              
+                         	      | $$            | $$            | $$              
+	 /$$$$$$$   /$$$$$$   /$$$$$$ | $$$$$$$   /$$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$ 
+	| $$__  $$ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$ |____  $$|_  $$_/   /$$__  $$
+	| $$  \ $$| $$  \ $$| $$  \ $$| $$  \ $$| $$  | $$  /$$$$$$$  | $$    | $$$$$$$$
+	| $$  | $$| $$  | $$| $$  | $$| $$  | $$| $$  | $$ /$$__  $$  | $$ /$$| $$_____/
+	| $$  | $$|  $$$$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$$|  $$$$$$$  |  $$$$/|  $$$$$$$
+	|__/  |__/ \______/  \______/ |_______/  \_______/ \_______/   \___/   \_______/
+                                                                                
+                                                                                                                                                     
+	 
 	Welcome to the decentralized blockchain Renaissance, above money & beyond cryptocurrency!
 	This script will update genesis_29-1 to genesis_29-2.
 	NOTE: Be ready to enter and remember your NEW strong passwords during the installation process.
@@ -32,19 +43,41 @@ cat << "EOF"
 EOF
 sleep 15s
 
-# SYSTEM UPDATE, INSTALLATION OF THE FOLLOWING PACKAGES: jq git wget make gcc build-essential snapd wget, INSTALLATION OF GO 1.17 via snap
+
+# SYSTEM UPDATE, INSTALLATION OF THE FOLLOWING PACKAGES: jq git wget make gcc build-essential snapd wget ponysay sl, INSTALLATION OF GO 1.17 via snap
 
 sudo apt-get update -y
-sudo apt-get install jq git wget make gcc build-essential snapd wget -y
+sudo apt-get install jq git wget make gcc build-essential snapd cmatrix sl wget -y
 snap install --channel=1.17/stable go --classic
 export PATH=$PATH:$(go env GOPATH)/bin
 echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
+snap install ponysay
 
 # GLOBAL CHANGE OF OPEN FILE LIMITS
 echo "* - nofile 50000" >> /etc/security/limits.conf
 echo "root - nofile 50000" >> /etc/security/limits.conf
 echo "fs.file-max = 50000" >> /etc/sysctl.conf 
 ulimit -n 50000
+
+ponysay "LAST REMINDER: This is an update script to migrate from genesis_29-1 to genesis_29-2 network! If you want new node from scratch, terminate this script with CTRL+C and run the install-genesisd.sh script"
+
+sleep 10s
+<< "EOF"
+
+     	    \\
+             \\_
+          .---(')
+        o( )_-\_   
+Follow the white rabbit                                                                                                                                                                                
+EOF
+sleep 5s
+cmatrix
+sleep 5s
+pkill cmatrix
+echo Now just updating the software! 
+#STOPPING EVMOSD DAEMON AND COSMOVISOR IF IT WAS NOT STOPPED
+pkill evmosd
+pkill cosmovisor
 
 # BACKUP genesis_29-1 .evmosd
 cd
@@ -79,17 +112,7 @@ genesisd unsafe-reset-all
 
 # STARTING GENESISL1 NEOLITHIC STAGE NODE
 genesisd start --chain-id genesis_29-2 --pruning=nothing --trace --log_level info --minimum-gas-prices=1el1
-echo All set! 
+echo All set, starting your updated node!
 sleep 3s
-
-
-cat << "EOF"
-
-     	    \\
-             \\_
-          .---(')
-        o( )_-\_
-       Node start                                                                                                                                                                                     
-EOF
- 
+sl
 sleep 5s
