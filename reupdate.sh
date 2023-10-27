648e07523d1bd40e1523085b3408aaaddfbfcb7d
zenodeapp <anodeofzen@gmail.com>
2023-10-27 23:35:40 +0200

    Added max_num_inbound_peers to 0

--- reupdate.sh
+++ reupdate.sh
@@ -146,6 +146,7 @@ sed -i 's/^seeds = .*/seeds = ""/' config.toml
 sed -i 's/^timeout_commit = .*/timeout_commit = "10s"/' config.toml
 sed -i 's/^persistent_peers = .*/persistent_peers = ""/' config.toml # TODO: manual peers list
 sed -i 's/^pex = .*/pex = false/g' config.toml
+sed -i 's/^max_num_inbound_peers = .*/max_num_inbound_peers = 0/g' config.toml
 
 sed -i 's/^minimum-gas-prices = .*/minimum-gas-prices = "50000000000el1"/g' app.toml
 sed -i 's/^halt-height = .*/halt-height = 6751390/g' app.toml
