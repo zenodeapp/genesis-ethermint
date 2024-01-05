85d8e44685a8f6229e3adc26fa01faf090b1d142
zenodeapp <zenodeapp@gmail.com>
2024-01-05 06:23:48 +0100

    Abstracted out dependencies and refactored scripts

--- scripts/quick-node-setup.sh
+++ scripts/quick-node-setup.sh
@@ -17,23 +17,18 @@
 # WARNING: This script does not create any backups whatsoever, make sure to create one if
 # you go this route.
 
+# Variables
+MONIKER=mygenesismoniker
+KEY=mygenesiskey
+CHAIN_ID=genesis_29-2
+NODE_DIR=.genesis
+
 # Stop processes
 systemctl stop genesisd
 pkill cosmovisor
 
 # System update and installation of dependencies
-sudo apt-get update -y
-sudo apt-get install jq git wget make gcc build-essential snapd wget -y
-snap install go --channel=1.20/stable --classic
-snap refresh go --channel=1.20/stable --classic
-export PATH=$PATH:$(go env GOPATH)/bin
-echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
-
-# Global change of open file limits
-echo "* - nofile 50000" >> /etc/security/limits.conf
-echo "root - nofile 50000" >> /etc/security/limits.conf
-echo "fs.file-max = 50000" >> /etc/sysctl.conf 
-ulimit -n 50000
+sh dependencies.sh
 
 # cd to root of the repository
 cd ..
@@ -42,21 +37,21 @@ cd ..
 make install
 
 # Set chain-id
-genesisd config chain-id genesis_29-2
+genesisd config chain-id $CHAIN_ID
 
 # Create key
 genesisd config keyring-backend os
-genesisd keys add mygenesiskey --keyring-backend os --algo eth_secp256k1
+genesisd keys add $KEY --keyring-backend os --algo eth_secp256k1
 
 # Init node
-genesisd init mygenesismoniker --chain-id genesis_29-2 -o
+genesisd init $MONIKER --chain-id $CHAIN_ID -o
 
 # State and chain specific configurations (i.e. timeout_commit 10s, min gas price 50gel).
-cp "./configs/default_app.toml" ~/.genesis/config/app.toml
-cp "./configs/default_config.toml" ~/.genesis/config/config.toml
-cp ./states/genesis_29-2/genesis.json ~/.genesis/config/genesis.json
+cp "./configs/default_app.toml" ~/$NODE_DIR/config/app.toml
+cp "./configs/default_config.toml" ~/$NODE_DIR/config/config.toml
+cp ./states/$CHAIN_ID/genesis.json ~/$NODE_DIR/config/genesis.json
 # Set moniker again since the configs got overwritten
-sed -i "s/moniker = .*/moniker = \"mygenesismoniker\"/" ~/.genesis/config/config.toml
+sed -i "s/moniker = .*/moniker = \"$MONIKER\"/" ~/$NODE_DIR/config/config.toml
 
 # Reset to imported genesis.json
 genesisd tendermint unsafe-reset-all
