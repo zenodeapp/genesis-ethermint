084b63a7d5b9fc1ff00afe712928ca7e6ccc6acd
zenodeapp <zenodeapp@gmail.com>
2024-01-05 03:44:27 +0100

    Renamed cmd/genesisd folder to cmd/tgenesisd

--- cmd/tgenesisd/root.go
+++ cmd/tgenesisd/root.go
@@ -64,7 +64,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   app.Name,
-		Short: "GenesisL1 Daemon",
+		Short: "GenesisL1 Testnet Daemon",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
