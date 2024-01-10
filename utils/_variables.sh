ceae2c061a5a074db12329a152f00f9556fabdb1
ZENODE <zenodeapp@gmail.com>
2024-01-10 10:23:37 +0100

    Update _variables.sh

--- utils/_variables.sh
+++ utils/_variables.sh
@@ -2,6 +2,7 @@
 
 CHAIN_ID=tgenesis_54-1
 BINARY_NAME=tgenesisd
-NODE_DIR=$HOME/.tgenesis
+NODE_DIR_NAME=.tgenesis
+NODE_DIR=$HOME/$NODE_DIR_NAME
 CONFIG_DIR=$NODE_DIR/config
 DATA_DIR=$NODE_DIR/data
