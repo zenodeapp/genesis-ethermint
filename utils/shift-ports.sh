af20403c1ad791061ad65715a4cd0b7723b31099
ZENODE <zenodeapp@gmail.com>
2024-01-13 23:55:12 +0100

    Updated shift-ports.sh to v1.0.1

--- utils/shift-ports.sh
+++ utils/shift-ports.sh
@@ -11,17 +11,17 @@ REPO_ROOT=$(cd "$(dirname "$0")"/.. && pwd)
 . "$REPO_ROOT/utils/_variables.sh"
 
 # Repository where the port shifter resides
-VERSION=v1.0.0 # Added versioning for non-breaking changes and security measures
+VERSION=v1.0.1 # Added versioning for non-breaking changes and security measures
 EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/port-shifter/$VERSION
 
 # quick-shift.sh (simple variant, $1 is <port_increment_value>, e.g.: 1000)
-# See https://github.com/zenodeapp/port-shifter/blob/v1.0.0/quick-shift.sh
+# See https://github.com/zenodeapp/port-shifter/blob/v1.0.1/quick-shift.sh
 curl -sO $EXTERNAL_REPO/quick-shift.sh
 sh ./quick-shift.sh "$CONFIG_DIR" $1
 rm quick-shift.sh
 
 # shift-wizard.sh (more advanced, if you prefer to edit individual ports)
-# See: https://github.com/zenodeapp/port-shifter/blob/v1.0.0/shift-wizard.sh
+# See: https://github.com/zenodeapp/port-shifter/blob/v1.0.1/shift-wizard.sh
 # curl -sO $EXTERNAL_REPO/shift-wizard.sh
 # sh ./shift-wizard.sh "$CONFIG_DIR"
 # rm shift-wizard.sh
